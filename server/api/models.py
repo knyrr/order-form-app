81a1ab5c92025b5309eed93536813ed2111ae49d
knyrr <rynk@tlu.ee>
2023-04-18 10:33:19 +0300

    Models updated

--- server/api/models.py
+++ server/api/models.py
@@ -13,3 +13,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class OrderForm(models.Model):
+    number = models.IntegerField
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date = models.DateField
+
+    def __str__(self):
+        return f"{self.number} {self.client.name}"
+
+
+class OrderFormLine(models.Model):
+    order_form = models.ForeignKey(OrderForm, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.DecimalField
+
+    def __str__(self):
+        return f"{self.order_form.number} {self.product} {self.quantity}"
+
+
+class Employee(models.Model):
+    email = models.EmailField
+
+    def __str__(self):
+        return self.email
