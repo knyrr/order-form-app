6d8a345691cc431ae28723bd01af6af1a0e5378d
knyrr <rynk@tlu.ee>
2023-04-18 11:34:39 +0300

    Backend running

--- server/api/models.py
+++ server/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 
 class Client(models.Model):
@@ -16,25 +17,27 @@ class Product(models.Model):
 
 
 class OrderForm(models.Model):
-    number = models.IntegerField
+    number = models.PositiveIntegerField(
+        unique=True, validators=[MaxValueValidator(9999999999)])
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    date = models.DateField
+    date = models.DateField()
 
     def __str__(self):
-        return f"{self.number} {self.client.name}"
+        return f"{self.number} {self.client.name} {self.date}"
 
 
 class OrderFormLine(models.Model):
     order_form = models.ForeignKey(OrderForm, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.DecimalField
+    quantity = models.DecimalField(max_digits=18, decimal_places=3, null=True)
 
     def __str__(self):
         return f"{self.order_form.number} {self.product} {self.quantity}"
 
 
 class Employee(models.Model):
-    email = models.EmailField
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
 
     def __str__(self):
         return self.email
